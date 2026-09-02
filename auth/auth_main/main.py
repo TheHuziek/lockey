f7fc8c7b3a23f87fe81bda07d8e0700643410cef
RNZLR-Dev <j.rmz.cortes@gmail.com>
2026-09-02 11:16:57 -0600

    actualizacion codigo auth

--- auth/auth_main/main.py
+++ auth/auth_main/main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+user = input("Ingrese su usuario: ")
+password = input("Ingrese su contraseña: ")
+
+
+if user == "jose@test.com":
+  if password == "jose-contra":
+    print("Acceso total")
+  else:
+    print("Acceso restringido")
+    if password != "jose-contra": 
+      print("Activa la verificacion en dos pasos")
+    elif password != "jose-contra":
+      print("Verificacion en dos pasos activada")
+else:
+  print("Acceso restringido")
