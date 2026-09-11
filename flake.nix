837a02da820a0fb6fa111c194d92b548b538be85
Thehuziek <thehuziek@proton.me>
2026-09-11 09:25:58 -0600

    change flake to add openssl in my system

--- flake.nix
+++ flake.nix
@@ -19,7 +19,11 @@
         rust-analyzer
         protobuf
       ];
+      buildInputs = [ pkgs.openssl ];
 
+      OPENSSL_DIR = "${pkgs.openssl.dev}";
+      OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+      OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
       # Variables de entorno para que los scripts build.rs (prost-build, tonic, etc.) encuentren protoc
       PROTOC = "${pkgs.protobuf}/bin/protoc";
       PROTOC_INCLUDE = "${pkgs.protobuf}/include";
